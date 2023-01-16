94670ac73b8c9e5c8aa1a5bd7135a2c01dae697f
Nearh777 <nearh777@gmail.com>
2023-01-16 15:05:27 +0200

    lesson 1

--- js/index.js
+++ js/index.js
@@ -1,1 +1,40 @@
-console.log('Привіт!');
+// Оголоси дві змінні, productName для назви товару, і pricePerItem для зберігання ціни за штуку. При оголошенні надай змінним наступні значення:
+
+// назва - рядок "Droid";
+// ціна за штуку - число 2000.
+// Оголошена змінна productName
+// Значення змінної productName - це рядок "Droid"
+// Оголошена змінна pricePerItem
+// Значення змінної pricePerItem - це число 2000
+// const productName = 'Droid';
+// const pricePerItem = 2000;
+// console.log(productName);
+// console.log(pricePerItem);
+
+
+//Напишіть цикл, який виводить у консоль
+// числа від max до min за спаданням
+// Додайте усі парні числа від min до max
+
+// const max = 50;
+
+// const min = 20;
+
+// let sum = 0;
+
+// for (let i = max; i >= min; i -= 1) {
+
+//     if (i % 2 !== 0) {
+
+//     continue;
+//   }
+//   sum += i;
+// }
+// console.log(sum);
+
+//Напишіть цикл, який виводить у консоль
+// числа від max до min за спаданням
+// Додайте усі парні числа від min до max
+const max = 50;
+const min = 20;
+
