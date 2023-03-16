5b5332a4c5d9da64493e4d34cfeba97dda9e1138
Nearh777 <nearh777@gmail.com>
2023-03-16 16:14:02 +0200

    apdated

--- js/index.js
+++ js/index.js
@@ -40,5 +40,35 @@
 // console.log(arr[1]);
 // console.log(arr[2]);
 
+//let a = '5' * 1 + '2' * 1;
+//alert(a);
+//let a = '5' * '1' + '2' * '1';
+//alert(a);
+//Запиши умову в інструкції if таким чином, щоб функція працювала правильно.
+
+//Оголошена функція checkAge(age).
+//У виразі перевірки віку використаний оператор >=
+//Виклик checkAge(20) повертає "You are an adult"
+//Виклик checkAge(8) повертає "You are a minor"
+//Виклик checkAge(14) повертає "You are a minor"
+//Виклик checkAge(38) повертає "You are an adult"
+//В тілі функції є тільки одна інструкція if
+//В тілі функції відсутні інструкції else або else if
+//function checkAge(age){
+//  if(age >= 18){
+//    return "You are an adult";
+//  }
+//  return "You are a minor";
+//}
 
 
+//function checkPassword(password) {
+//  const ADMIN_PASSWORD = "jqueryismyjam";
+//  // Change code below this line
+//  if (password === ADMIN_PASSWORD) {
+//     return "Welcome!" ;
+//     }
+//   return "Access denied, wrong password!";
+  
+//    // Change code above this line
+//}
