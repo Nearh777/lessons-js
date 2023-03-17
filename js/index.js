b58d007be3b46e2a5efbb6726f971717de386966
Nearh777 <nearh777@gmail.com>
2023-03-17 17:07:32 +0200

    updated

--- js/index.js
+++ js/index.js
@@ -1,29 +1,29 @@
-// Создайте переменную age и присвойте ей ваш возраст. 
+// Создайте переменную age и присвойте ей ваш возраст.
 // Выведите на экран 'Мне %Возраст% лет!'.
 
 // const age = 44;
 // console.log('Мені', (`${age}`), 'років!');
 
-// Спросите имя пользователя с помощью методы prompt. 
+// Спросите имя пользователя с помощью методы prompt.
 // Выведите с помощью alert сообщение 'Ваше имя %имя%'.
 
 // const userName = prompt('Введіть Ваше імя');
 // console.log('Ваше імя:', (`${userName}`));
 
-// Спросите у пользователя число. 
+// Спросите у пользователя число.
 // Выведите с помощью alert квадрат этого числа.
 
 // const number = prompt('Введіть число');
 // const numberSqr = Math.pow(`${number}`, 2);
 // console.log('Введене число в квадраті:', (`${numberSqr}`));
 
-// Создайте переменную str и присвойте ей значение 'abcde'. 
-// Обращаясь к отдельным символам этой строки выведите на экран 
+// Создайте переменную str и присвойте ей значение 'abcde'.
+// Обращаясь к отдельным символам этой строки выведите на экран
 // символ 'a', символ 'c', символ 'e'.
 // const str = 'abcde';
 // console.log(str[0], str[2], str[4]);
 
-// Создайте переменную num и присвойте ей значение '12345'. 
+// Создайте переменную num и присвойте ей значение '12345'.
 // Найдите произведение (умножение) цифр этого числа.
 // const num = '12345';
 // const num1 = Number(num);
@@ -31,7 +31,7 @@
 
 // console.log(num2);
 
-//  Создайте массив arr = ['a', 'b', 'c']. 
+//  Создайте массив arr = ['a', 'b', 'c'].
 //  Выведите его на экран с помощью функции alert.
 
 // const arr = ['a', 'b', 'c'];
@@ -61,7 +61,6 @@
 //  return "You are a minor";
 //}
 
-
 //function checkPassword(password) {
 //  const ADMIN_PASSWORD = "jqueryismyjam";
 //  // Change code below this line
@@ -69,7 +68,7 @@
 //     return "Welcome!" ;
 //     }
 //   return "Access denied, wrong password!";
-  
+
 //    // Change code above this line
 //}
 // let a = 2 * (8 / 4);
@@ -86,7 +85,7 @@
 //   // Change code below this line
 //   if (ordered === 0) {
 //      return "Your order is empty!";
-//   } 
+//   }
 //   if (ordered > available) {
 //      return  "Your order is too large, not enough goods in stock!";
 //   } //else {
@@ -116,37 +115,35 @@
 //     // Change code below this line
 //     let firstArrayElement = array[0];
 //     let lastArrayElement = array[array.length - 1];
-//     let newArray = [firstArrayElement, lastArrayElement]; 
-//     
+//     let newArray = [firstArrayElement, lastArrayElement];
+//
 //     return newArray;
-  
+
 //     // Change code above this line
 //   }
-  
+
 // function splitMessage(message, delimeter) {
 //     let words = message.split( delimeter);
 //     // Change code below this line
 //     return words;
 //     // Change code above this line
-     
+
 //   }
 
 // function calculateEngravingPrice(message, pricePerWord) {
 //     // Change code below this line
 //  const arrayMessage = message.split(' ');
 //  const totalPrice = arrayMessage.length * pricePerWord;;
-//  return totalPrice; 
+//  return totalPrice;
 
 //   // Change code above this line
-// } 
-
+// }
 
 // function makeStringFromArray(array, delimeter) {
 //     let string;
 //     // Change code below this line
 //     string = array.join(delimeter);
-  
-  
+
 //     // Change code above this line
 //     return string;
 //   }
@@ -156,8 +153,7 @@
 //     const titleLowerCase = title.toLowerCase();
 //     const slug = titleLowerCase.split(" ");
 //    return slug.join('-');
-  
-  
+
 //     // Change code above this line
 //   }
 
@@ -173,7 +169,6 @@
 
 // const allClients = oldClients.concat(newClients); // Change this line
 
-
 // function makeArray(firstArray, secondArray, maxLength) {
 //     const allArray = firstArray.concat(secondArray);
 //     if (allArray.length > maxLength) {
@@ -182,29 +177,26 @@
 //       return allArray;
 //   }
 
-
 //    // Change code above this line
 //  }
-
 
 // const start = 3;
 // const end = 7;
 
 // for (let i = start; i <= end; i +=1 ) {
-  
-//     console.log(i);    
-//     //break;
- 
-// }
 
+//     console.log(i);
+//     //break;
+
+// }
 
 // function calculateTotal(number) {
 //     // Change code below this line
 //      let total = 0;
 //     for (let i=0, sum=0; i <= number; i +=1 ) {
 //       total += i;
-//     } 
-//      return total;    
+//     }
+//      return total;
 //       }
 
 // const fruits = ['apple', 'plum', 'pear', 'orange'];
@@ -218,26 +210,32 @@
 //     let total = 0;
 //     for (i = 0; i < order.length; i ++) {
 //         //const order = array[i];
-//         total += order[i];     
+//         total += order[i];
 //          }
 //          return total;
 //     }
 //   // return total;
 //     // Change code above this line
-   
-function findLongestWord(string) {
-    // Change code below this line
-    let arrString = string.split(' ');
-    let wordLength = 0;
-    let longestWord;
-    for (let i = 0; i < arrString .length; i += 1) {
-          wordLength = arrString [1].length;
-  
-      if (arrString [i].length > wordLength) {
-           longestWord = arrString [i];
-          return longestWord;
-        }
-     }
-  
-    // Change code above this line
-  }
+
+// function findLongestWord(string) {
+//   // Change code below this line
+//   let arrString = string.split(' ');
+//   let wordLength = 0;
+//   let longestWord;
+//   for (let i = 0; i < arrString.length; i += 1) {
+//     wordLength = arrString[1].length;
+
+//     if (arrString[i].length > wordLength) {
+//       longestWord = arrString[i];
+//       return longestWord;
+//     }
+//   }
+
+//   // Change code above this line
+// }
+
+// console.log(
+//   findLongestWord(
+//     'Напишите программу, которая выводит через все числа от 1 до 100, с двумя исключениями. '
+//   )
+// );
