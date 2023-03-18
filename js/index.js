49ae6aa4978458db3391fd07f19a01f924bc45bf
Nearh777 <nearh777@gmail.com>
2023-03-18 17:12:43 +0200

    updated

--- js/index.js
+++ js/index.js
@@ -426,7 +426,28 @@
 //   return message;
 // }
 
+
+
 // Об'єкти
+
+// 
+// const products = [
+//   { name: "Radar", price: 1300, quantity: 4 },
+//   { name: "Scanner", price: 2700, quantity: 3 },
+//   { name: "Droid", price: 400, quantity: 7 },
+//   { name: "Grip", price: 1200, quantity: 9 },
+// ];
+// function getProductPrice(productName) {
+//   // Change code below this line
+  
+//    for (let product of products) {
+//     if (productName === product.name)  {
+//       return product.price; 
+//        }
+         
+//   }
+//   return null;
+// }
 
 
 
